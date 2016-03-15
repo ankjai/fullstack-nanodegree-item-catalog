b9256fc04f7c88c9a2f47e002a70918959294b3a
ankjai <ankit.jaiswal@appdirect.com>
2016-03-15 14:42:23 -0700

    login/logout link at home.

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -127,6 +127,11 @@ def gconnect():
 
 
 @app.route('/logout')
+def show_logout():
+    return render_template('logout.html')
+
+
+@app.route('/gdisconnect', methods=['POST'])
 def gdisconnect():
     access_token = login_session.get('access_token')
     if access_token is None:
@@ -137,21 +142,17 @@ def gdisconnect():
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
-    print 'result is '
-    print result
     if result['status'] == '200':
         del login_session['access_token']
         del login_session['gplus_id']
         del login_session['username']
         del login_session['email']
         del login_session['picture']
-        response = make_response(json.dumps('Successfully disconnected.'), 200)
-        response.headers['Content-Type'] = 'application/json'
-        return response
+        flash("logged out.", category='success')
+        return redirect('/restaurant')
     else:
-        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
-        response.headers['Content-Type'] = 'application/json'
-        return response
+        flash('Failed to revoke token for given user.', category='error')
+        return redirect('/restaurant')
 
 
 @app.route('/')
